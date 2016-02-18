1e157b26db9de5e18b946bf377248f9dd53bb0ed
sarojkh <alt.sarojk@gmail.com>
2016-02-18 13:46:03 +0545

    Commented out tests not required. Also, added TODO in tests that need to be implemented.

--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -5,45 +5,53 @@ class GroupsControllerTest < ActionController::TestCase
     @group = groups(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:groups)
-  end
+# TODO
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
+  #   assert_not_nil assigns(:groups)
+  # end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+# TODO
+  # test "should get new" do
+  #   get :new
+  #   assert_response :success
+  # end
 
-  test "should create group" do
-    assert_difference('Group.count') do
-      post :create, group: {  }
-    end
+# TODO
+  # test "should create group" do
+  #   assert_difference('Group.count') do
+  #     post :create, group: {  }
+  #   end
+  #
+  #   assert_redirected_to group_path(assigns(:group))
+  # end
 
-    assert_redirected_to group_path(assigns(:group))
-  end
+# TODO
+  # test "should show group" do
+  #   get :show, id: @group
+  #   assert_response :success
+  # end
 
-  test "should show group" do
-    get :show, id: @group
-    assert_response :success
-  end
+# TODO
+  # test "should get edit" do
+  #   get :edit, id: @group
+  #   assert_response :success
+  # end
 
-  test "should get edit" do
-    get :edit, id: @group
-    assert_response :success
-  end
+# TODO
+  # test "should update group" do
+  #   patch :update, id: @group, group: {  }
+  #   assert_redirected_to group_path(assigns(:group))
+  # end
 
-  test "should update group" do
-    patch :update, id: @group, group: {  }
-    assert_redirected_to group_path(assigns(:group))
-  end
-
-  test "should destroy group" do
-    assert_difference('Group.count', -1) do
-      delete :destroy, id: @group
-    end
-
-    assert_redirected_to groups_path
-  end
+# Currently group destroy will not be implemented.
+# However, soft-deletion might be implemented, if desired by client in the future.
+  # test "should destroy group" do
+  #   assert_difference('Group.count', -1) do
+  #     delete :destroy, id: @group
+  #   end
+  #
+  #   assert_redirected_to groups_path
+  # end
 end
