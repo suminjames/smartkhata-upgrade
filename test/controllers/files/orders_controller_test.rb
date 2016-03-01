76e8a35f6cc1f4f582cf0d368b27577fa188a65b
Subas <subas@danpheit.com>
2016-03-01 07:33:33 +0000

    added few tests and changed layout of the application

--- test/controllers/files/orders_controller_test.rb
+++ test/controllers/files/orders_controller_test.rb
@@ -1,12 +1,14 @@
 require 'test_helper'
 
-class Files::FloorsheetsControllerTest < ActionController::TestCase
+class Files::OrdersControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
   test "should get new"  do
     get :new
     assert_response :success
-    assert_select ".section-title", "Upload FloorSheet file"
+    assert_select ".section-title", "Upload Order file"
   end
+
 end
--- test/controllers/files/sales_controller_test.rb
+++ test/controllers/files/sales_controller_test.rb
@@ -4,4 +4,9 @@ class Files::SalesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should get new"  do
+    get :new
+    assert_response :success
+    assert_select ".section-title", "Upload Sales CM file"
+  end
 end
