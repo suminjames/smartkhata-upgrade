b582a703bc6f1b5ba83a06cc8106df4ea0824f97
sarojkh <alt.sarojk@gmail.com>
2016-04-01 15:43:34 +0545

    Errors are indispensable.

--- nepali_calendar-1.0.1/lib/nepali_calendar/bs.rb
+++
@@ -1,9 +0,0 @@
-module NepaliCalendar
-  BS = {}
-  File.open(File.dirname(__FILE__) + '/bs.txt') do |f|
-    f.each_line do |line|
-      key, value = line.split(' => ')
-      BS[key.to_i] = value[1..-3].split(', ').map(&:to_i)
-    end
-  end
-end
--- nepali_calendar-1.0.1/lib/nepali_calendar/version.rb
+++
@@ -1,3 +0,0 @@
-module NepaliCalendar
-  VERSION = '1.0.1'
-end
