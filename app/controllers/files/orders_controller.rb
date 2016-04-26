8848a0fe45a54ed3c95a28f25fad58e1bb8e7be9
Subas poudel <poudel.subas089@gmail.com>
2016-04-26 11:47:16 +0545

    minor fixes for instance variable, deleted unused helpers

--- app/controllers/files/orders_controller.rb
+++ app/controllers/files/orders_controller.rb
@@ -1,30 +1,57 @@
 class Files::OrdersController < ApplicationController
 	# 	@@file = FileUpload::FILES[:order];
 
+	@@file = FileUpload::FILES[:floorsheet];
+	@@file_name_contains = "ORDER"
+
 	def new
 	end
 
 	def import
-		# authorize self
-		@file = params[:file];
+    # get file from import
+    @file = params[:file];
 
+    # read the xls file
+    xlsx = Roo::Spreadsheet.open(@file)
 
-		if @file == nil
-			flash.now[:error] = "Please Upload a valid file"
-			@error = true
-		else
-			begin
-			  xlsx = Roo::Spreadsheet.open(@file, extension: :xlsx)
-			rescue Zip::Error
-			  xlsx = Roo::Spreadsheet.open(@file)
-			end
+    # array to store processed data
+    @processed_data = []
+    @raw_data = []
 
-			@x = Date.parse(xlsx.sheet(0).row(5)[9].tr('()',""))
-			(15..(xlsx.sheet(0).last_row)).each do |i|
+    # loop through 13th row to last row
+    # parse the data
+    @total_amount = 0
+    count = 0
+    # header = xlsx.sheet(0).row(11)
+    # (14..(xlsx.sheet(0).last_row)).each do |i|
+    #   row = Hash[[header, xlsx.sheet(0).row(i)].transpose]
+    #   count += 1
+    #   @row_data = row.to_hash
+    #   puts @row_data
+    #   break if count > 5
+    # end
 
-				break if xlsx.sheet(0).row(i)[0] == nil
-				puts xlsx.sheet(0).row(i).inspect
-			end
-		end
-	end
+    xlsx.sheet(0).each(
+        id: 'Order ID',
+        symbol: 'Symbol',
+        client_name: 'Client Name',
+        client_code: 'Client Code',
+        price: 'Price',
+        quantity: 'Quantity',
+        amount: 'Amount',
+        pending_qty: 'Pending Quantity',
+        order_time: 'Order Time',
+        order_type: 'Order Type',
+        order_segment: 'Order Segment',
+        order_condition: 'Order Condition',
+        # order_condition: 'Order Condition',
+        order_state: 'Order State'
+    ) do |hash|
+      count += 1
+      puts hash.inspect
+      break if count > 5
+      # => { id: 1, name: 'John Smith' }
+    end
+
+  end
 end
--- app/helpers/share_inventories_helper.rb
+++
@@ -1,2 +0,0 @@
-module ShareInventoriesHelper
-end
