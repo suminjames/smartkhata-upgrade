dd2d5e6f62d3ce70bbf718b3b74cc4a75bd97417
Subas poudel <poudel.subas089@gmail.com>
2016-05-03 11:40:04 +0545

    fixed issues with voucher, bill, settlement

--- app/globalhelpers/number_formatter_module.rb
+++ app/globalhelpers/number_formatter_module.rb
@@ -6,7 +6,8 @@ module NumberFormatterModule
 		word = decimal.to_f.to_words
 		if word.kind_of?(Array)
       word_before_decimal = word[0].titleize
-      word_before_decimal = "#{word_before_decimal.sub! 'And', ''} Rupees"
+			word_before_decimal = ( word_before_decimal.sub! 'And', '' ) || word_before_decimal
+      word_before_decimal = "#{word_before_decimal} Rupees"
 			word_after_decimal = "And #{paisa}/100" if paisa > 0
 			word = "#{word_before_decimal} #{word_after_decimal}"
 		else
