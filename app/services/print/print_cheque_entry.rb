1ecaf3a44eb2521293bd86f3dff71edbb79d30f3
sarojkh <alt.sarojk@gmail.com>
2016-05-26 10:31:55 +0545

    Cheque print completed.

--- app/services/print/print_cheque_entry.rb
+++ app/services/print/print_cheque_entry.rb
@@ -5,18 +5,50 @@ class Print::PrintChequeEntry < Prawn::Document
   include ApplicationHelper
 
   # def initialize(voucher, particulars, bank_account, cheque, current_tenant)
-  def initialize(cheque, current_tenant)
+  def initialize(cheque_entry, name, cheque_date, current_tenant)
     super(top_margin: 1, right_margin: 18, bottom_margin: 18, left_margin: 18)
 
-    @cheque = cheque
+    @beneficiary_name = name
+    @cheque_date = cheque_date
+    @cheque_entry = cheque_entry
     @current_tenant = current_tenant
 
     draw
   end
 
   def draw
+    # Test Data BEGINS
+    # ac_payee_note = 'A/C Payee'
+    # date = Date.today
+    # beneficiary_name = 'Warnakulasuriya Patabendige Ushantha Joseph Chaminda Vaas'
+    # amount_in_number = 999999999
+    # amount_in_word = arabic_word(amount_in_number)
+    # amount_in_number = arabic_number(999999999)
+    # Test Data ENDS
+
+    ac_payee_note = 'A/C Payee'
+    date = @cheque_date.strftime("%d-%m-%Y")
+    beneficiary_name = @beneficiary_name
+    amount_in_number = @cheque_entry.amount
+    amount_in_word = arabic_word(amount_in_number) +' only'
+    amount_in_number = arabic_number(amount_in_number)
+
+    # Dimensions
+    cheque_top = page_height
+    cheque_left = 0
+
     font_size(9) do
-    #   TODO(sarojk)
+      # Left (to the perforation) side of the cheque
+      text_box '2519', :at => [cheque_left + 0.9.cm, cheque_top - 1.cm], :width => 2.6.cm # TODO(sarojk) unknown placeholder. apparently has '2519'
+      text_box date.to_s, :at => [cheque_left + 0.9.cm, cheque_top - 1.4.cm], :width => 2.6.cm
+      text_box beneficiary_name, :at => [cheque_left + 0.9.cm, cheque_top - 1.9.cm], :width => 2.6.cm
+      text_box amount_in_number.to_s, :at => [cheque_left + 0.9.cm, cheque_top - 4.5.cm], :width => 2.6.cm
+      # Right (to the perforation) side of the cheque
+      text_box ac_payee_note, :at => [cheque_left + 11.2.cm, cheque_top - 1.1.cm]
+      text_box date.to_s, :at => [cheque_left + 17.9.cm, cheque_top - 1.3.cm]
+      text_box beneficiary_name, :at => [cheque_left + 8.8.cm, cheque_top - 2.2.cm], :width => 7.5.cm
+      text_box amount_in_number.to_s, :at => [cheque_left + 17.9.cm, cheque_top - 3.1.cm]
+      text_box amount_in_word, :at => [cheque_left + 6.1.cm, cheque_top - 2.9.cm], :width => 10.2.cm
     end
   end
 
