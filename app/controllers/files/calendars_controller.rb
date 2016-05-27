782677fa0eff0327b2b811aedd9ee18486c7a150
sarojkh <alt.sarojk@gmail.com>
2016-05-27 16:22:22 +0545

    got test snippet running.

--- app/controllers/files/calendars_controller.rb
+++ app/controllers/files/calendars_controller.rb
@@ -23,13 +23,10 @@ class Files::CalendarsController < ApplicationController
     # - Grab the last modified timestamp of the file uploaded
     # - Check the file upload date (different from modified timestamp of the file)
 
-    # cal_1 = NepaliCalendar::Calendar.new
-    # cal_1 = NepaliCalendarPlus::CalendarPlus.new
-    cal_1 = NepaliCalendarPlus::CalendarPlus.new
-    abort
+    @cal =  NepaliCalendarPlus::CalendarPlus.new
 
     from_date_ad = @cal.bs_to_ad(2073, 1, 1)
-    to_date_ad = @cal.bs_to_ad(2074, 12, 31)
+    to_date_ad = @cal.bs_to_ad(2074, 12, 30)
 
     from_date_ad.upto(to_date_ad) do |ad_date|
       bs_date = @cal.ad_to_bs(ad_date.year, ad_date.month, ad_date.day)
@@ -39,6 +36,7 @@ class Files::CalendarsController < ApplicationController
         date_hash[:month] =  bs_date[:month]
         date_hash[:day] = bs_date[:day]
         date_hash[:date_type] = 'x'
+        date_hash[:ad_date] = ad_date.to_s
         if ad_date.saturday?
           date_hash[:date_type] = 'Saturday'
           # hash[:remarks]: 'Remarks',
@@ -59,8 +57,9 @@ class Files::CalendarsController < ApplicationController
     ) do |hash|
       #The column headers are at row 3. So skip those 3 rows.
       if count > 3
-        date =  Date.parse(hash[:year].to_i.to_s + "-"  + hash[:month].to_i.to_s + "-" + hash[:day].to_i.to_s)
-        unless date_already_in_db (date)
+        # date =  Date.parse(hash[:year].to_i.to_s + "-"  + hash[:month].to_i.to_s + "-" + hash[:day].to_i.to_s)
+        date =  {:year => hash[:year], :month => hash[:month], :day => hash[:day]}
+        unless bs_date_already_in_db? (date)
           hash[:is_holiday] = true
           Calendar.create(hash)
         end
@@ -71,7 +70,7 @@ class Files::CalendarsController < ApplicationController
 
   # Checks the passed date against the 'Calendars' table in the database
   def bs_date_already_in_db? (date)
-    Calendar.where(year: date.year, month: date.month, day: date.day).count != 0
+    Calendar.where(year: date[:year], month: date[:month], day: date[:day]).count != 0
   end
 end
 
