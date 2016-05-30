63493567b6cc7cb1081824fc87df4ad063f9e522
sarojkh <alt.sarojk@gmail.com>
2016-05-30 18:03:17 +0545

    Changed cell bordering.

--- app/services/print/print_voucher.rb
+++ app/services/print/print_voucher.rb
@@ -113,6 +113,10 @@ class Print::PrintVoucher< Prawn::Document
       t.header = true
       t.cell_style = {:border_width => 1, :padding => [1,2,1,2], :align => :left}
       t.row(0).font_style = :bold
+      t.columns(0..-1).borders = [:left]
+      t.columns(-1).borders = [:left, :right]
+      t.rows(0).borders = [:top, :bottom, :left, :right]
+      t.rows(-1).borders = [:bottom, :left, :right]
       t.column_widths = column_widths
     end
   end
@@ -134,6 +138,10 @@ class Print::PrintVoucher< Prawn::Document
       t.header = true
       t.cell_style = {:border_width => 1, :padding => [1,2,1,2], :align => :left}
       t.row(0).font_style = :bold
+      t.columns(0..-1).borders = [:left]
+      t.columns(-1).borders = [:left, :right]
+      t.rows(0).borders = [:top, :bottom, :left, :right]
+      t.rows(-1).borders = [:bottom, :left, :right]
       t.column_widths = column_widths
     end
 
