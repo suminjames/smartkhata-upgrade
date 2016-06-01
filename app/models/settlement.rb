749c18fc891f5297116687a5cddd68f2364803c6
Subas poudel <poudel.subas089@gmail.com>
2016-06-01 13:21:08 +0545

    added payment and receipt list

--- app/models/settlement.rb
+++ app/models/settlement.rb
@@ -31,4 +31,5 @@ class Settlement < ActiveRecord::Base
   # to keep track of the user who created and last updated the ledger
   belongs_to :creator,  class_name: 'User'
   belongs_to :updater,  class_name: 'User'
+
 end
