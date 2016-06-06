3935a373ddfcdfddde1d1b2e951f45f8c69f988d
sarojkh <alt.sarojk@gmail.com>
2016-06-06 14:50:04 +0545

    whatever.

--- app/models/settlement.rb
+++ app/models/settlement.rb
@@ -31,11 +31,7 @@ class Settlement < ActiveRecord::Base
   belongs_to :vendor_account
 
   scope :by_settlement_type, -> (type) { where(:settlement_type => Settlement.settlement_types[type]) }
-<<<<<<< Updated upstream
   scope :by_date, -> (date) { where(:created_at => date.beginning_of_day..date.end_of_day) }
-=======
-  scope :by_date, -> (date) { where(:create_ad => date.beginning_of_day..date.end_of_day) }
->>>>>>> Stashed changes
   scope :by_date_range, -> (date_from, date_to) { where( :date => date_from.beginning_of_day..date_to.end_of_day) }
   scope :by_client_id, -> (id) { where(client_account_id: id) }
   scope :by_vendor_id, -> (id) { where(vendor_account_id: id) }
