1a68299a1e57790e15f648c6dbfc237a7d0a0367
Subas poudel <poudel.subas089@gmail.com>
2016-06-07 11:33:54 +0545

    changes to search clients accounts and their group members, added scope to get client account with group members

--- app/models/client_account.rb
+++ app/models/client_account.rb
@@ -72,6 +72,7 @@ class ClientAccount < ActiveRecord::Base
 
   after_create :create_ledger
   belongs_to :group_leader,  class_name: 'ClientAccount'
+  has_many :group_members, :class_name => 'ClientAccount', :foreign_key => 'group_leader_id'
 
 	belongs_to :user
 
@@ -84,7 +85,9 @@ class ClientAccount < ActiveRecord::Base
 	scope :find_by_client_id, -> (id) { where(id: id) }
   scope :find_by_boid, -> (boid) { where("boid" => "#{boid}") }
   scope :get_existing_referrers_names, -> { where.not(referrer_name: '').select(:referrer_name).distinct}
-
+  # for future reference only .. delete if you feel you know things well enough
+  # scope :having_group_members, includes(:group_members).where.not(group_members_client_accounts: {id: nil})
+  scope :having_group_members, joins(:group_members)
 	enum client_type: [:individual, :corporate ]
 
   # create client ledger
