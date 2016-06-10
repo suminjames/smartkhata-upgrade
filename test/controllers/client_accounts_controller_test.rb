16449af57a9547d2999db1b0d952eaed476e8dae
TwilightCoder <idiot.gorkhali@gmail.com>
2016-06-10 17:40:51 +0545

    Roughly wrote Employee accounts controller test; Briefly refactored Client accounts controller test; Added data in client account fixture for the new validations

--- test/controllers/client_accounts_controller_test.rb
+++ test/controllers/client_accounts_controller_test.rb
@@ -4,48 +4,49 @@ class ClientAccountsControllerTest < ActionController::TestCase
   def setup
     sign_in users(:user)
     @client_account = client_accounts(:one)
+    @block_assert = lambda{ |action|
+      instance_var = action == :index ? :client_accounts : :client_account
+      assert_response :success
+      assert_template "client_accounts/#{action}"
+      assert_not_nil assigns(instance_var)
+    }
   end
 
   test "should get new" do
     get :new
-    assert_response :success
-    assert_not_nil assigns(:client_account)
+    @block_assert.call(:new)
   end
 
   # Search by certain params maybe?
   test "should get index" do
     get :index
     assert_redirected_to client_accounts_path(search_by: "name")
-
     get :index, search_by: 'name'
-    assert_response :success
-    assert_not_nil assigns(:client_accounts)
+    @block_assert.call(:index)
   end
 
   test "should show client account" do
     get :show, id: @client_account
-    assert_response :success
-    assert_not_nil assigns(:client_account)
+    @block_assert.call(:show)
   end
 
   test "should get edit" do
     get :edit, id: @client_account
-    assert_response :success
-    assert_not_nil assigns(:client_account)
+    @block_assert.call(:edit)
   end
 
   test "should create new" do
     assert_difference 'ClientAccount.count', 1 do
-      post :create, client_account: {name: ''}
+      post :create, client_account: {name: 'New Client', citizen_passport: '123456', dob: '1900-01-01', father_mother: 'foo', granfather_father_inlaw: 'bar',
+                                     address1_perm: 'baz', city_perm: 'alpha', state_perm: 'beta', country_perm: 'gamma'}
     end
     assert_redirected_to client_account_path(assigns(:client_account))
   end
 
   test "should update client account" do
     assert_not_equal 'Minitest', @client_account.name
-    post :update, id: @client_account, client_account: {name: 'Minitest'}
+    patch :update, id: @client_account, client_account: {name: 'Minitest'}
     assert_redirected_to client_account_path(assigns(:client_account))
-    assert_not_nil assigns(:client_account)
     @client_account.reload
     assert_equal 'Minitest', @client_account.name
   end
