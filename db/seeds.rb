064dbfd443688c0db01e9119ab44d05a94424321
subas45 <poudel.subas089@gmail.com>
2016-06-22 15:17:46 +0545

    pukar testing space

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,33 @@
 
 
 
-tenant = Tenant.find_or_create_by!(name: "dipshikha", dp_id: '11000')
-tenant.update(full_name: 'Dipshikha Dhitopatra Karobar Company Pvt. Ltd.', address: 'Anamnagar, Kathmandu', phone_number: '977-1-4444444', fax_number: '977-1-4444444', pan_number: '55555555', broker_code: '38')
-tenant = Tenant.find_or_create_by!(name: "trishakti", dp_id: '11400')
-tenant.update(full_name: 'Trishakti Securities Public Ltd.', address: 'Putalisadak, Kathmandu', phone_number: '977-1-4232132', fax_number: '977-1-4232133', pan_number: '302830905', broker_code: '48')
-tenant = Tenant.find_or_create_by!(name: "public", dp_id: '11400')
-tenant.update(full_name: 'Trishakti Securities Public Ltd.', address: 'Putalisadak, Kathmandu', phone_number: '977-1-4232132', fax_number: '977-1-4232133', pan_number: '302830905', broker_code: '48')
-@tenants = Tenant.all
+# tenant = Tenant.find_or_create_by!(name: "dipshikha", dp_id: '11000')
+# tenant.update(full_name: 'Dipshikha Dhitopatra Karobar Company Pvt. Ltd.', address: 'Anamnagar, Kathmandu', phone_number: '977-1-4444444', fax_number: '977-1-4444444', pan_number: '55555555', broker_code: '38')
+# tenant = Tenant.find_or_create_by!(name: "trishakti", dp_id: '11400')
+# tenant.update(full_name: 'Trishakti Securities Public Ltd.', address: 'Putalisadak, Kathmandu', phone_number: '977-1-4232132', fax_number: '977-1-4232133', pan_number: '302830905', broker_code: '48')
+# tenant = Tenant.find_or_create_by!(name: "public", dp_id: '11400')
+# tenant.update(full_name: 'Trishakti Securities Public Ltd.', address: 'Putalisadak, Kathmandu', phone_number: '977-1-4232132', fax_number: '977-1-4232133', pan_number: '302830905', broker_code: '48')
+# @tenants = Tenant.all
 
+
+@tenants = []
+tenant = Tenant.find_or_create_by!(name: "demo", dp_id: '1010')
+tenant.update(full_name: 'Danphe InfoTech Private Ltd.', address: 'Kupondole, Lalitpur', phone_number: '977-1-4232132', fax_number: '977-1-4232133', pan_number: '302830905', broker_code: '00')
+@tenants << tenant
+tenant = Tenant.find_or_create_by!(name: "danphe", dp_id: '1010')
+tenant.update(full_name: 'Danphe InfoTech Private Ltd.', address: 'Kupondole, Lalitpur', phone_number: '977-1-4232132', fax_number: '977-1-4232133', pan_number: '302830905', broker_code: '00')
+@tenants << tenant
+
+
+# @admin_users = [
+#     {:email => 'dipshikha@danfeinfotech.com', :password => 'dipshikha5645'},
+#     {:email => 'trishakti@danfeinfotech.com', :password => 'trispa8934'},
+#     {:email => 'trishakti@danfeinfotech.com', :password => 'trispa8934'}
+# ]
 
 @admin_users = [
-    {:email => 'dipshikha@danfeinfotech.com', :password => 'dipshikha5645'},
-    {:email => 'trishakti@danfeinfotech.com', :password => 'trispa8934'},
-    {:email => 'trishakti@danfeinfotech.com', :password => 'trispa8934'}
+    {:email => 'demo@danfeinfotech.com', :password => '12demo09'},
+    {:email => 'danphe@danfeinfotech.com', :password => '32Dan89'},
 ]
 
 count = 0
---
+++ lib/tasks/db.rake
@@ -0,0 +1,37 @@
+# lib/tasks/db.rake
+namespace :db do
+
+  desc "Dumps the database to db/APP_NAME.dump"
+  task :dump => :environment do
+    cmd = nil
+    with_config do |app, host, db, user|
+      cmd = "pg_dump --host #{host} --username #{user} --verbose --clean --no-owner --no-acl --format=c #{db} > #{Rails.root}/db/backup/#{Time.now.strftime("%Y%m%d%H%M%S")}_#{db}.psql"
+    end
+    puts cmd
+    exec cmd
+  end
+
+  desc "Restores the database from backups"
+    task :restore, [:date] => :environment do |task,args|
+        if args.date.present?
+            cmd = nil
+            with_config do |app, host, db, user|
+              cmd = "pg_restore --verbose --host #{host} --username #{user} --clean --no-owner --no-acl --dbname #{db} #{Rails.root}/db/backup/#{args.date}_#{db}.psql"
+            end
+            Rake::Task["db:drop"].invoke
+            Rake::Task["db:create"].invoke
+            puts cmd
+            exec cmd
+        else
+            puts 'Please pass a date to the task'
+        end
+    end
+  private
+
+  def with_config
+    yield Rails.application.class.parent_name.underscore,
+      ActiveRecord::Base.connection_config[:host],
+      ActiveRecord::Base.connection_config[:database],
+      ActiveRecord::Base.connection_config[:username]
+  end
+end
