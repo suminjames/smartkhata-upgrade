94179df693b470362aa7aa51803a45c6d82dac6d
subas45 <poudel.subas089@gmail.com>
2016-06-24 16:03:21 +0545

    added policies for bill and ledger

---
+++ app/policies/ledger_policy.rb
@@ -0,0 +1,3 @@
+class LedgerPolicy < ApplicationPolicy
+  permit_access_to_employee_and_above :index, :show, :new, :edit, :create, :update, :destroy, :group_members_ledgers, :transfer_group_member_balance
+end
