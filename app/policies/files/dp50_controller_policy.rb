07e5fb5dd3e992949a84e659090812bbcf5c234d
subas45 <poudel.subas089@gmail.com>
2016-06-27 12:38:26 +0545

    added file client nepse mapping

---
+++ app/policies/files/dp50_controller_policy.rb
@@ -0,0 +1,20 @@
+class Files::Dp50ControllerPolicy
+	attr_reader :current_user, :ctlr
+
+  def initialize(current_user, ctlr)
+    @current_user = current_user
+    @ctlr = ctlr
+  end
+
+  def new?
+    @current_user.admin? or @current_user.manager? or @current_user.supervisor?
+  end
+
+  def import?
+    @current_user.admin? or @current_user.manager? or @current_user.supervisor?
+  end
+
+  def index?
+    @current_user.admin? or @current_user.manager? or @current_user.supervisor?
+  end
+end
