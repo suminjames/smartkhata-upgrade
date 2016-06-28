cd0213bfb31e063e5449dc465ab227fc27b84f14
sarojkh <alt.sarojk@gmail.com>
2016-06-28 11:19:18 +0545

    Got hardworker to handle Email.

--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -3,10 +3,7 @@ class EmailWorker
   sidekiq_options :retry => true
 
   def perform(bill_id, current_tenant_id)
-    @current_tenant = Tenant.find_by_id(current_tenant_id)
-    Apartment::Tenant.switch!(@current_tenant.name)
-    UserMailer.bill_email(bill_id, current_tenant_id)
-    Apartment::Tenant.switch!('public')
+    UserMailer.delay.bill_email(bill_id, current_tenant_id)
   end
 
 end
