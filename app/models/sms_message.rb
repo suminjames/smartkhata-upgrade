4f20bfee5c48ab8b8b91591121a11756e369a5ac
sarojkh <alt.sarojk@gmail.com>
2016-07-04 15:46:39 +0545

    Stash.

--- app/models/sms_message.rb
+++ app/models/sms_message.rb
@@ -25,8 +25,10 @@ class SmsMessage < ActiveRecord::Base
   belongs_to :transaction_message
 
   # Expand the enum type to add additional types in the future (For eg: password change PIN sms, etc)
-  enum sms_type: [:undefined_sms_type, :transaction_sms]
+  enum sms_type: [:undefined_sms_type, :transaction_message_sms]
   enum phone_type: [:undefined_phone_type, :ntc, :ncell]
+
+  attr_accessor :remarks, :credit_used
 
   @access_code = 'M210DAF977'
   @username = 'danpheit'
@@ -44,30 +46,17 @@ class SmsMessage < ActiveRecord::Base
   #  8 = Invalid values in parameters like date incorrect format, mobile != 13 digits
   #  9 = Balance not enough.
 
-  def initialize (phone, sms_type, remarks, transaction_message_id)
-    @phone = phone
-    @sms_type = sms_type
-    @credit_used = self.credit_used
-    @remarks = remarks || ''
-    @transaction_message_id = transaction_message_id
-    @phone_type = self.phone_type(@phone)
+  def initialize (args = {})
+    p 'Initialized'
+    super()
+    self.phone = args[:phone]
+    self.sms_type = args[:sms_type]
+    self.transaction_message_id = args[:transaction_message_id]
+    p 'PHone type called'
+    self.phone_type = self.class.get_phone_type(self.phone)
+    self.credit_used = 0
   end
 
-  # As per Miracle Infocom's sendable pattern: 13 digits (prefix: 977) 984,985,986,980,981,974,975
-  # ntc_prefixes = ['984', '985', '986']
-  # ncell_prefixes = ['980', '981']
-  # TODO(sarojk): Find which provider has prefixes 974, 975?
-  def self.phone_type(phone)
-    phone = self.manipulate_phone_number(phone)
-    non_area_code_segment = phone.split('977')[1]
-    if non_area_code_segment.starts_with?('984', '985', '986')
-      return SmsMessage.phone_types[:ntc]
-    elsif non_area_code_segment.starts_with?('980', '981')
-      return SmsMessage.phone_types[:ncell]
-    else
-      return SmsMessage.phone_types[:undefined_phone_type]
-    end
-  end
 
   def self.check_balance
     tag = 'BQ'
@@ -77,27 +66,21 @@ class SmsMessage < ActiveRecord::Base
     result.split('=')[1].strip.to_f
   end
 
-  def self.send_hello_world(current_tenant_id)
-    @current_tenant = Tenant.find_by_id(current_tenant_id)
-    Apartment::Tenant.switch!(@current_tenant.name)
-    @date_time = DateTime.now.to_s(:db)
-    self.mobile_number = '9845817957'
-    @message = 'a' * 251
-    p @message
-    p @message.length
-    p self.push_sms
-    p self.check_balance
-    Apartment::Tenant.switch!('public')
-  end
-
-  def self.send_message_gt_255
-    @message = "Yesterday all my troubles seemed so far away. Now it looks as though they're here to stay. Oh, I believe in yesterday. Suddenly I'm not half the man I used to be. There's a shadow hanging over me. Oh, yesterday came suddenly. Why she had to go, I don't know, she wouldn't say. I said something wrong, now I long for yesterday."
-  end
-
   # The reply_code is a string
   def self.push_sms
     tag = 'B'
     reply_code = Net::HTTP.get_response(URI.parse('http://api.miracleinfo.com.np/sms/smssend.php?'+ 'tag=' + tag + '&ac=' + @access_code + '&dt=' + @date_time + '&mob=' + @mobile_number + '&msg=' + @message + '&u=' + @username + '&p=' + @password)).body
+  end
+
+  def self.send_hello_world(current_tenant_id)
+    @current_tenant = Tenant.find_by_id(current_tenant_id)
+    Apartment::Tenant.switch!(@current_tenant.name)
+    self.date_time
+    self.mobile_number = '9851153385'
+    self.message = 'Hello, from DIT!'
+    p self.push_sms
+    p self.check_balance
+    Apartment::Tenant.switch!('public')
   end
 
   def self.send_bill_sms(transaction_message_id, current_tenant_id )
@@ -105,16 +88,38 @@ class SmsMessage < ActiveRecord::Base
     Apartment::Tenant.switch!(@current_tenant.name)
     self.date_time
     transaction_message = TransactionMessage.find_by(id: transaction_message_id.to_i)
-    transaction_message.sms_queued!
-    self.message = transaction_message.sms_message
-    self.date_time
     self.mobile_number = transaction_message.client_account.messageable_phone_number
-    reply_code = self.push_sms
-    if reply_code == '1'
+    self.date_time
+    sms_message_obj = SmsMessage.new(phone: @mobile_number, sms_type: SmsMessage.sms_types[:transaction_message_sms], transaction_message_id: transaction_message.id)
+    #  -split message to sendable blocks(<=255)
+    #  -queue all blocks
+    #  -in the event one of the blocks fails, unsent! but increase the credit used. remark 'multiple block' message
+    #   -else, if none fails, register sent!
+    #TODO(sarojk): Consider sane breaking of text . Looks like , and ; are sane break points.
+    # http://stackoverflow.com/questions/754407/what-is-the-best-way-to-chop-a-string-into-chunks-of-a-given-length-in-ruby
+    valid_message_blocks = transaction_message.sms_message.scan(/.{1,255}/)
+    sms_failed = false
+    transaction_message.sms_queued!
+    valid_message_blocks.each do |message|
+      self.message = message
+      # reply_code = self.push_sms
+      reply_code = '1'
+      if reply_code != '1'
+        sms_failed = true
+        if transaction_message.sms_message.length > 255
+          sms_message_obj.remarks = 'Not all valid length blocks succesfully sent of this message which is greater than 255 characters.'
+        end
+        break
+      else
+        sms_message_obj.credit_used += credit_required(@message)
+      end
+    end
+    if sms_failed
+      transaction_message.sms_unsent!
+    else
       transaction_message.increase_sent_sms_count!
       transaction_message.sms_sent!
-    else
-      transaction_message.sms_unsent!
+      sms_message_obj.save!
     end
     Apartment::Tenant.switch!('public')
   end
@@ -128,6 +133,7 @@ class SmsMessage < ActiveRecord::Base
     @mobile_number = self.manipulate_phone_number(number)
   end
 
+  # :db used to get it in
   def self.date_time
     @date_time = DateTime.now.to_s(:db)
   end
@@ -137,6 +143,7 @@ class SmsMessage < ActiveRecord::Base
   def self.manipulate_phone_number(number)
     number = number.to_s.gsub(/\D/, '')
     number = number.prepend('977') unless number.start_with?('977')
+    number
   end
 
   # Checks for number validity as per Miracle Infocom's sendable pattern: 13 digits (prefix: 977) 984,985,986,980,981,974,975
@@ -147,18 +154,39 @@ class SmsMessage < ActiveRecord::Base
     return non_area_code_segment.present? && non_area_code_segment.length == 10 && non_area_code_segment.starts_with?('984', '985', '986', '980', '981', '974', '975')
   end
 
-  def self.credit_used
+  # Valid message block: 255 characters
+  # <= 160 characters = 1 credit
+  # > 160 characters && <= 255 characters = +1 credit
+  # When a message with 255+ characters is broken to multiple blocks and sent, the credit required is calculated as below.
+  def self.credit_required(message)
     credit = 0
-    case @message.length
-      when 0
-        credit = 0
-      when 1..160
-        credit = 1
-      when 160..255
-        credit = 2
-      else
-        credit = 0
+    message_length = message.length
+    while message_length > 0
+      if message_length > 160
+        credit += 2
+        message_length -= 255
+      elsif message_length <= 160
+        credit += 1
+        message_length -= 160
+      end
     end
     credit
   end
+
+  # As per Miracle Infocom's sendable pattern: 13 digits (prefix: 977) 984,985,986,980,981,974,975
+  # ntc_prefixes = ['984', '985', '986']
+  # ncell_prefixes = ['980', '981']
+  # TODO(sarojk): Find which provider has prefixes 974, 975?
+  def self.get_phone_type(phone)
+    phone = self.manipulate_phone_number(phone)
+    non_area_code_segment = phone.split('977')[1]
+    if non_area_code_segment.starts_with?('984', '985', '986')
+      return SmsMessage.phone_types[:ntc]
+    elsif non_area_code_segment.starts_with?('980', '981')
+      return SmsMessage.phone_types[:ncell]
+    else
+      return SmsMessage.phone_types[:undefined_phone_type]
+    end
+  end
+
 end
