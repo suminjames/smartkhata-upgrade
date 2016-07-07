c19713ca6490b18fa777d19873ade72c93dc27b7
subas45 <poudel.subas089@gmail.com>
2016-07-07 23:20:38 +0545

    ledger balance storage

---
+++ app/models/ledger_balance.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: ledger_balances
+#
+#  id           :integer          not null, primary key
+#  opening_blnc :decimal(15, 4)   default("0.0")
+#  closing_blnc :decimal(15, 4)   default("0.0")
+#  fy_code      :integer
+#  branch_id    :integer
+#  ledger_id    :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
+class LedgerBalance < ActiveRecord::Base
+  belongs_to :ledger
+  include ::Models::UpdaterWithFyCode
+end
