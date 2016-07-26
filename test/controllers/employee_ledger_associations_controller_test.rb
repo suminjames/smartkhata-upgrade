3e359b1f473f391fa728f9c30fee2ec470366e84
TwilightCoder <idiot.gorkhali@gmail.com>
2016-07-26 12:10:28 +0545

    Fixed voucher fixtures that caused an issue in floorsheet upload. Also wrote/updated general settings & employee ledger association controller tests.

--- test/controllers/employee_ledger_associations_controller_test.rb
+++ test/controllers/employee_ledger_associations_controller_test.rb
@@ -1,38 +1,41 @@
-=begin
+
 require 'test_helper'
 
 class EmployeeLedgerAssociationsControllerTest < ActionController::TestCase
   setup do
+    sign_in users(:user)
     @employee_ledger_association = employee_ledger_associations(:one)
+    @block_assert = lambda{ |action|
+      params = [:show, :edit].include?(action) ? {id: @employee_ledger_association} : {}
+      get action, params
+      instance_var = action == :index ? :employee_ledger_associations : :employee_ledger_association
+      assert_response :success
+      assert_template "employee_ledger_associations/#{action}"
+      assert_not_nil assigns(instance_var)
+    }
   end
 
   test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:employee_ledger_associations)
+    @block_assert.call(:index)
   end
 
   test "should get new" do
-    get :new
-    assert_response :success
+    @block_assert.call(:new)
   end
 
   test "should create employee_ledger_association" do
     assert_difference('EmployeeLedgerAssociation.count') do
       post :create, employee_ledger_association: {  }
     end
-
     assert_redirected_to employee_ledger_association_path(assigns(:employee_ledger_association))
   end
 
   test "should show employee_ledger_association" do
-    get :show, id: @employee_ledger_association
-    assert_response :success
+    @block_assert.call(:show)
   end
 
   test "should get edit" do
-    get :edit, id: @employee_ledger_association
-    assert_response :success
+    @block_assert.call(:edit)
   end
 
   test "should update employee_ledger_association" do
@@ -44,8 +47,6 @@ class EmployeeLedgerAssociationsControllerTest < ActionController::TestCase
     assert_difference('EmployeeLedgerAssociation.count', -1) do
       delete :destroy, id: @employee_ledger_association
     end
-
     assert_redirected_to employee_ledger_associations_path
   end
 end
-=end
