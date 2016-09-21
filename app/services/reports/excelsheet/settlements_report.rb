f9426b4f228792d73cfeff07026784afd8283e1b
TwilightCoder <idiot.gorkhali@gmail.com>
2016-09-21 14:20:44 +0545

    Added sn, bank and cheque number columns to report- from view

--- app/services/reports/excelsheet/settlements_report.rb
+++ app/services/reports/excelsheet/settlements_report.rb
@@ -1,5 +1,5 @@
 class Reports::Excelsheet::SettlementsReport < Reports::Excelsheet
-  TABLE_HEADER = ["Name", "Amount", "Date", "Description", "Type"]
+  TABLE_HEADER = ["SN.", "Name", "Amount", "Bank", "Cheque Number", "Date", "Description", "Type"]
 
   def initialize(settlements, params, current_tenant)
     super(settlements, params, current_tenant)
@@ -14,15 +14,17 @@ class Reports::Excelsheet::SettlementsReport < Reports::Excelsheet
 
   def prepare_document
     # Conditionally adds document headings and sets the filename
-    headings = ["Payment/Receipt Report"]
-    @file_name = "SettlementReport"
+    settlement_type = case @settlement_type
+    when 'payment', 'receipt'
+      @settlement_type.capitalize
+    else
+      'Payment/Receipt'
+    end
+    headings = ["#{settlement_type} Report"]
+    @file_name = "#{settlement_type.sub('/', '')}Report"
     if @client_account
       headings << "\"#{@client_account.name.strip}\""
       @file_name << "_#{@client_account.id}"
-    end
-    if @settlement_type
-      headings << "Settlement Type: #{@settlement_type}"
-      @file_name << "_#{@settlement_type}"
     end
     @file_name << "_#{@date}"
     add_document_headings(headings)
@@ -54,21 +56,39 @@ class Reports::Excelsheet::SettlementsReport < Reports::Excelsheet
 
   def populate_data_rows
     # inserts the actual data rows through iteration.
-    normal_style_row = ([@styles[:wrap]]*4).insert(1, @styles[:float_format])
-    striped_style_row = ([@styles[:wrap_striped]]*4).insert(1, @styles[:float_format_striped])
+    normal_style_row = [@styles[:normal_center]].push(*[@styles[:wrap]]*6).insert(2, @styles[:float_format])
+    striped_style_row = [@styles[:striped_center]].push(*[@styles[:wrap_striped]]*6).insert(2, @styles[:float_format_striped])
+    # debugger
     @settlements.each_with_index do |s, index|
+      sn = index + 1
+      # copy+pasted from view (_list)
+      cheque_number = nil
+      bank_name = nil
+      if s.voucher.cheque_entries.present?
+        s.voucher.cheque_entries.uniq.each do |cheque|
+          if s.has_single_cheque? && cheque.client_account_id == s.client_account_id || !s.has_single_cheque?
+            cheque_number = cheque.cheque_number
+            bank_name = cheque.receipt? ? cheque.additional_bank.name : cheque.bank_account.bank_name
+          end
+        end
+      end
+      # 'N/A' for non_bank payment/receipt which don't have cheque_entry associated
+      cheque_number ||= 'N/A'
+      bank_name ||= 'N/A'
+
       settlement_type = s.receipt? ? 'receipt' : 'payment'
+
       row_style = index.even? ? normal_style_row : striped_style_row
-      # debugger
-      @sheet.add_row [s.name, s.amount, s.date_bs, s.description, settlement_type], style: row_style
+      @sheet.add_row [sn, s.name, s.amount, bank_name, cheque_number, s.date_bs, s.description, settlement_type], style: row_style
     end
   end
 
   def set_column_widths
     # Sets fixed widths for a few required columns
     # Fixed width for first column which may be elongated by document headers
-    @sheet.column_info.first.width = 30
+    # @sheet.column_info.first.width = 5
 
-    # sheet.column_widths 6, nil, nil, nil
+    # problems with auto width
+    @sheet.column_widths 6, nil, nil, 25, 18
   end
 end
