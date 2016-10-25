b1a0e8c6890c66358acc1b7d9c702c99693b1ed9
sarojkh <alt.sarojk@gmail.com>
2016-10-25 16:21:36 +0545

    Capital Gain PDF implemented.

--- app/controllers/share_transactions_controller.rb
+++ app/controllers/share_transactions_controller.rb
@@ -128,8 +128,37 @@ class ShareTransactionsController < ApplicationController
     ) or return
 
     client_id = params.dig(:filterrific, :by_client_id)
-    @share_transactions = ShareTransaction.capital_gain_transactions_by_client_id(client_id).page(params[:page]).per(20)
+    @share_transactions = ShareTransaction.capital_gain_transactions_by_client_id(client_id).includes(:isin_info, :bill, :client_account).decorate
 
+    @download_path_xlsx = capital_gain_report_share_transactions_path({format:'xlsx'}.merge params)
+    @download_path_pdf = capital_gain_report_share_transactions_path({format:'pdf'}.merge params)
+
+    respond_to do |format|
+      format.html
+      format.js
+      # format.xlsx do
+      # end
+      format.pdf do
+        pdf = Reports::Pdf::CustomerCapitalGainReport.new(@share_transactions, current_tenant, {:print_in_letter_head => params[:print_in_letter_head]})
+        send_data pdf.render, filename: "CapitalGainReport_#{@share_transactions.first.client_account.nepse_code}.pdf", type: 'application/pdf', disposition: :inline
+      end
+    end
+
+  rescue RuntimeError => e
+    puts "Had to reset filterrific params: #{ e.message }"
+    respond_to do |format|
+      flash.now[:error] = 'One of the search options provided is invalid.'
+      format.html { render :index }
+      format.json { render json: flash.now[:error], status: :unprocessable_entity }
+    end
+
+      # Recover from invalid param sets, e.g., when a filter refers to the
+      # database id of a record that doesn’t exist any more.
+      # In this case we reset filterrific and discard all filter params.
+  rescue ActiveRecord::RecordNotFound => e
+    # There is an issue with the persisted param_set. Reset it.
+    puts "Had to reset filterrific params: #{ e.message }"
+    redirect_to(reset_filterrific_url(format: :html)) and return
   end
 
   def pending_deal_cancel
