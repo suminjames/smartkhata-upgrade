a99fa251a6a62e1b1eb8298648fdc0166dbca2cf
sarojkh <alt.sarojk@gmail.com>
2016-11-03 11:25:51 +0545

    Fixed a client invitation glitch. Also, adjusted dashboard for client to accomodate multiple associated client accounts.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,6 @@ class DashboardController < ApplicationController
     # CreateMenuItemsService.new.call
     # return
 
-
     @total_users = ClientAccount.count
 
     @balance = Group.pnl
@@ -40,14 +39,20 @@ class DashboardController < ApplicationController
 
   def client_index
     authorize :dashboard, :index?
-    client_id  = 2078
-    @client_account = ClientAccount.find_by_id(client_id)
-    @client_info_hash = {}
-    @client_info_hash[:pending_bills_count] = @client_account.bills.pending.size
-    @client_info_hash[:pending_bills_path] = @client_account.pending_bills_path
-    @client_info_hash[:ledger_closing_balance] = @client_account.ledger_closing_balance
-    @client_info_hash[:grouped_share_inventories] = ShareInventory.group_by_isin_for_client(@client_account.id)
-    @client_info_hash[:share_inventory_path] = @client_account.share_inventory_path
+    # Hold information for multiple associated client accounts for current logged in user.
+    @clients_info_arr = []
+    client_accounts = current_user.client_accounts.order(:name)
+    client_accounts.each do |client_account|
+      client_account = ClientAccount.find_by_id(client_account.id)
+      client_info_hash = {}
+      client_info_hash[:client_account] = client_account
+      client_info_hash[:pending_bills_count] = client_account.bills.pending.size
+      client_info_hash[:pending_bills_path] = client_account.pending_bills_path
+      client_info_hash[:ledger_closing_balance] = client_account.ledger_closing_balance
+      client_info_hash[:grouped_share_inventories] = ShareInventory.group_by_isin_for_client(client_account.id)
+      client_info_hash[:share_inventory_path] = client_account.share_inventory_path
+      @clients_info_arr << client_info_hash
+    end
   end
 
 end
