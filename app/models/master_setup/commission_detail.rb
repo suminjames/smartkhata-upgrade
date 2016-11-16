15fc66bce032db93162d661d59b07309b33c2c99
subas45 <poudel.subas089@gmail.com>
2016-11-16 23:43:20 +0545

    validation for commission rates

--- app/models/master_setup/commission_detail.rb
+++ app/models/master_setup/commission_detail.rb
@@ -1,7 +1,32 @@
 class MasterSetup::CommissionDetail < ActiveRecord::Base
   # belongs_to :master_setup_commission_info
+  ########################################
+  # Constants
+  MAX = 99999999999
+  MIN = 0
+
+
+  ########################################
+  # Callbacks
+  before_validation :set_max_min_amount
 
   ########################################
   # Vaidations
-  validates_presence_of :start_amount, :limit_amount
+  validates_presence_of :start_amount, unless: :limit_amount?
+  validates_presence_of :limit_amount, unless: :start_amount?
+  validates_presence_of :commission_rate, unless: :commission_amount?
+  validate :validate_amounts
+  ########################################
+  # Methods
+  private
+  def validate_amounts
+    if self.start_amount > self.limit_amount
+      errors.add :start_amount, "Starting price cant be less than the limit"
+    end
+  end
+
+  def set_max_min_amount
+    self.limit_amount = MAX if self.limit_amount.blank?
+    self.start_amount = 0 if self.start_amount.blank?
+  end
 end
--- app/models/master_setup/commission_info.rb
+++ app/models/master_setup/commission_info.rb
@@ -10,7 +10,14 @@ class MasterSetup::CommissionInfo < ActiveRecord::Base
 
   ########################################
   # Vaidations
+  validate :validate_date_range
   validate :validate_details
+
+  ########################################
+  # Constants
+  MAX = 99999999999
+  MIN = 0
+
 
   ########################################
   # Scopes
@@ -20,6 +27,38 @@ class MasterSetup::CommissionInfo < ActiveRecord::Base
 
   private
 
+  def validate_date_range
+    commission_info_latest = self.class.all.order(:start_date => :desc).first
+    if start_date >= end_date
+      errors.add :start_date, "Start date should be before the end date"
+      return
+    end
+
+    if commission_info_latest.present?
+      errors.add :base, "Entry missing for dates before the starting date" if self.start_date.yesterday != commission_info_latest.end_date
+    end
+  end
+
   def validate_details
+    starting_amounts = []
+    limit_amounts = []
+    self.commission_details.each do |d|
+      starting_amounts << d.start_amount
+      limit_amounts << d.limit_amount
+    end
+    starting_amounts.sort!
+    limit_amounts.sort!
+
+
+    # all should be unique
+    errors.add :base, "Invalid Data" if starting_amounts.size > starting_amounts.uniq.size
+    errors.add :base, "Invalid Data" if limit_amounts.size > limit_amounts.uniq.size
+    errors.add :base, "At least one detail should be present and have min amount" if starting_amounts[0] != 0
+    errors.add :base, "At least one detail should address the max amount" if limit_amounts[-1] != MAX
+
+    starting_amounts = starting_amounts.drop(1)
+    limit_amounts.pop
+    errors.add :base, "Invalid Data" if limit_amounts != starting_amounts
+
   end
 end
