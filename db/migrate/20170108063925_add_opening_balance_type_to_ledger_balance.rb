5eb60bd6d49cf2b018ecfb434ad815fd491fd53e
sarojkh <alt.sarojk@gmail.com>
2017-01-10 11:45:22 +0545

    Un-did the changes made to this file in earlier commit.

--- db/migrate/20170108063925_add_opening_balance_type_to_ledger_balance.rb
+++ db/migrate/20170108063925_add_opening_balance_type_to_ledger_balance.rb
@@ -1,5 +1,5 @@
 class AddOpeningBalanceTypeToLedgerBalance < ActiveRecord::Migration
   def change
-    add_column :ledger_balances, :opening_balance_type, :integer
+    add_column :ledger_balances, :opening_balance_type, :integer, default: 0
   end
 end
