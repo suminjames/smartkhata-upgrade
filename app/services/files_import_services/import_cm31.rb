c718064754858d8df895540848560a88a1adce79
sarojkh <alt.sarojk@gmail.com>
2017-01-16 12:39:25 +0545

    Included cm31 new to menu.

--- app/services/files_import_services/import_cm31.rb
+++ app/services/files_import_services/import_cm31.rb
@@ -121,7 +121,7 @@ class FilesImportServices::ImportCm31 < ImportFile
         # return list of sales settlement ids
         # that track the file uploads for different settlement
         settlement_ids.each do |settlement_id|
-          @nepse_settlement_ids << NepsePurchaseSettlement.find_or_create_by!(settlement_id: settlement_id).id
+          @nepse_settlement_ids << NepsePurchaseSettlement.find_or_create_by!(settlement_id: settlement_id, status: NepseSettlement.statuses[:complete]).id
         end
       end
     end
