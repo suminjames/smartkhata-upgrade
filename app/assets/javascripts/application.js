0f1d4824192f857cd947371c1cf72974008e8512
subas45 <poudel.subas089@gmail.com>
2017-04-08 23:08:59 +0545

    added react

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
@@ -23,6 +25,7 @@
 //= require select2.min.js
 //= require react
 //= require react_ujs
+//= require react-bootstrap
 //= require components
 //= require_tree .
 
