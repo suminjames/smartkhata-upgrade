7f7e4ff754a80f557be67416d20af9aa1932ab66
sarojkh <alt.sarojk@gmail.com>
2017-04-20 16:43:10 +0545

    beneficiary name filter included in cheque entry index.

--- app/controllers/cheque_entries_controller.rb
+++ app/controllers/cheque_entries_controller.rb
@@ -19,6 +19,7 @@ class ChequeEntriesController < ApplicationController
         params[:filterrific],
         select_options: {
             by_client_id: ClientAccount.options_for_client_select(params[:filterrific]),
+            by_beneficiary_name: ChequeEntry.options_for_beneficiary_name(params[:filterrific]),
             by_bank_account_id: ChequeEntry.options_for_bank_account_select,
             by_cheque_entry_status: ChequeEntry.options_for_cheque_entry_status,
             by_cheque_issued_type: ChequeEntry.options_for_cheque_issued_type
@@ -330,6 +331,21 @@ class ChequeEntriesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to cheque_entries_url, notice: 'Cheque entry was successfully destroyed.' }
       format.json { head :no_content }
+    end
+  end
+
+  #
+  # Entertains Ajax request made by combobox used in various views to populate clients.
+  #
+  def combobox_ajax_filter_for_beneficiary_name
+    search_term = params[:q]
+    beneficiary_names = []
+    # 3 is the minimum search_term length to invoke find_similar_to_name
+    if search_term && search_term.length >= 3
+      beneficiary_names = ChequeEntry.find_beneficiary_name_similar_to_term(search_term)
+    end
+    respond_to do |format|
+      format.json { render json: beneficiary_names, status: :ok }
     end
   end
 
