dcde014a17ce446ec0d17e7bfa7a5d1989bccadc
subas45 <poudel.subas089@gmail.com>
2017-07-19 22:58:22 +0545

    spec changes

--- spec/services/accounts/bills/change_date_service_spec.rb
+++ spec/services/accounts/bills/change_date_service_spec.rb
@@ -34,11 +34,11 @@ describe Accounts::Bills::ChangeDateService do
 
   describe '.process' do
     context "when sales bill" do
-      let(:bill) {create(:sales_bill, net_amount: 115130.6726, date: '2017-05-29', branch_id: branch.id ) }
-      let(:voucher) { create(:voucher, date_bs:  '2074-02-15', branch_id: branch.id)}
+      let(:bill) {create(:sales_bill, net_amount: 115130.6726, date: '2017-05-29', branch_id: @branch.id ) }
+      let(:voucher) { create(:voucher, date_bs:  '2074-02-15', branch_id: @branch.id)}
       let(:sales_share_transaction) {create(:sales_share_transaction_processed, bill: bill)}
-      let(:dr_particular)  { create(:debit_particular, voucher: voucher, branch_id: branch.id)}
-      let(:cr_particular)  { create(:credit_particular, voucher: voucher, branch_id: branch.id)}
+      let(:dr_particular)  { create(:debit_particular, voucher: voucher, branch_id: @branch.id)}
+      let(:cr_particular)  { create(:credit_particular, voucher: voucher, branch_id: @branch.id)}
 
       before do
         sales_share_transaction
@@ -50,12 +50,12 @@ describe Accounts::Bills::ChangeDateService do
 
 
       it "should change the dates of the bills" do
-        UserSession.selected_branch_id = branch.id
+        UserSession.selected_branch_id = @branch.id
         expect(sales_share_transaction.reload.bill.date).to eq('2017-05-28'.to_date)
       end
 
       it "should change voucher date" do
-        UserSession.selected_branch_id = branch.id
+        UserSession.selected_branch_id = @branch.id
         expect(voucher.reload.date).to eq('2017-05-28'.to_date)
       end
 
@@ -65,7 +65,7 @@ describe Accounts::Bills::ChangeDateService do
       end
 
       it "should patch ledger dailies" do
-        UserSession.selected_branch_id = branch.id
+        UserSession.selected_branch_id = @branch.id
         ledger = dr_particular.ledger.reload
         expect(ledger.ledger_dailies.where(date: '2017-05-29').first).to eq(nil)
         expect(ledger.reload.ledger_dailies.where(date: '2017-05-28').first.closing_balance).to eq(dr_particular.amount)
