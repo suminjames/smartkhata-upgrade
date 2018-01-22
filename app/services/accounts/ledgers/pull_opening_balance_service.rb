266f983d693aa581c0dcba5bc610ebc03e52d568
subas45 <poudel.subas089@gmail.com>
2018-01-22 13:48:16 +0545

    branch moving

--- app/services/accounts/ledgers/pull_opening_balance_service.rb
+++ app/services/accounts/ledgers/pull_opening_balance_service.rb
@@ -2,34 +2,38 @@ module Accounts
   module Ledgers
     class PullOpeningBalanceService
       include FiscalYearModule
-      attr_accessor :branch_id, :fy_code
+      attr_accessor :branch_id, :fy_code, :ledger_ids
 
-      def initialize(branch_id: nil, fy_code: nil )
+      def initialize(branch_id: nil, fy_code: nil, ledger_ids: [] )
         @branch_id = branch_id
         @fy_code = fy_code || get_fy_code
+        @ledger_ids = ledger_ids
       end
 
       def process
         branch_ids = Branch.all.pluck(:id)
-        if branch_id
-          ledger_ids = Ledger.where(client_account_id: ClientAccount.where(branch_id: branch_id).pluck(:id)).pluck(:id)
-        else
-          ledger_ids = Ledger.all.pluck(:id)
+        available_ledger_ids = ledger_ids;
+        unless available_ledger_ids.size > 0
+          if branch_id
+            available_ledger_ids = Ledger.where(client_account_id: ClientAccount.where(branch_id: branch_id).pluck(:id)).pluck(:id)
+          else
+            available_ledger_ids = Ledger.all.pluck(:id)
+          end
         end
+
         previous_fy_code = get_previous_fy_code fy_code
         pulled_ledger_ids = []
         pulled_ledger_names = []
-        Ledger.where(id: ledger_ids).find_each do |ledger|
+        Ledger.where(id: available_ledger_ids).find_each do |ledger|
           has_closing_balance = false
           LedgerBalance.unscoped.where(fy_code: previous_fy_code, ledger_id: ledger.id).find_each do |ledger_balance|
-            if ledger_balance.closing_balance != 0
-              lb = LedgerBalance.unscoped.find_or_create_by!(fy_code: fy_code, ledger_id: ledger_balance.ledger_id, branch_id: ledger_balance.branch_id)
-              if (lb.opening_balance !=  ledger_balance.closing_balance)
-                lb.update_attributes(opening_balance: ledger_balance.closing_balance, opening_balance_type: ledger_balance.closing_balance >= 0 ? 'dr': 'cr')
-                has_closing_balance = true
-              end
+            lb = LedgerBalance.unscoped.find_or_create_by!(fy_code: fy_code, ledger_id: ledger_balance.ledger_id, branch_id: ledger_balance.branch_id)
+            if (lb.opening_balance !=  ledger_balance.closing_balance)
+              lb.update_attributes(opening_balance: ledger_balance.closing_balance, opening_balance_type: ledger_balance.closing_balance >= 0 ? 'dr': 'cr')
+              has_closing_balance = true
             end
           end
+
           pulled_ledger_ids << ledger.id if has_closing_balance
           pulled_ledger_names << ledger.name if has_closing_balance
         end
--- spec/services/accounts/branches/client_branch_service_spec.rb
+++ spec/services/accounts/branches/client_branch_service_spec.rb
@@ -7,6 +7,7 @@ describe Accounts::Branches::ClientBranchService do
     @client_account = create(:client_account, name: "John", branch_id: 1)
     @other_client_account = create(:client_account, name: "preeti", branch_id: 1)
     @ledger = create(:ledger, client_account_id: @client_account.id, branch_id: 1)
+    @cash_ledger= Ledger.find_or_create_by(name: "Cash")
     @ledger_balance = create(:ledger_balance, ledger_id: @ledger.id, branch_id: 1, fy_code: 7475)
     @ledger_daily = create(:ledger_daily, ledger_id: @ledger.id, branch_id: 1, fy_code: 7475, date: '2017-9-16')
     @other_ledger = create(:ledger, client_account_id: @other_client_account.id, branch_id: 1)
@@ -15,18 +16,35 @@ describe Accounts::Branches::ClientBranchService do
     @other_particular = create(:particular, voucher_id: @voucher.id, transaction_date: '2017-10-16', ledger_id: @other_ledger.id, branch_id: 1)
     @bill = create(:bill, client_account_id: @client_account.id, date: '2017-9-16', branch_id: 1, fy_code: 7475)
     @settlement = create(:settlement, client_account_id: @client_account.id, date: '2017-9-16', branch_id: 1, fy_code: 7475)
-    @client_account.bills << @bill
+    # @client_account.bills << @bill
     subject {Accounts::Branches::ClientBranchService.new}
   end
   describe 'move transactions' do
-    it 'should move transactions' do
-      UserSession.selected_fy_code = 7475
-      subject.move_transactions(@client_account, 2, nil)
-      expect(Bill.unscoped.where(client_account_id: @client_account.id).first.branch_id).to eq(2)
-      expect(Settlement.where(client_account_id: @client_account.id).first.branch_id).to eq(2)
-      expect(@client_account.ledger.particulars.first.branch_id).to eq(2)
-      expect(@client_account.ledger.particulars.first.voucher.branch_id).to eq(1)
+    context "when voucher does not have other client particulars" do
+      it 'should move all particulars of the voucher' do
+        UserSession.selected_fy_code = 7475
+        @other_particular.update_attributes(ledger_id: @cash_ledger.id)
+        subject.move_transactions(@client_account, 2, nil, false)
+        expect(Bill.unscoped.where(client_account_id: @client_account.id).first.branch_id).to eq(2)
+        expect(Settlement.where(client_account_id: @client_account.id).first.branch_id).to eq(2)
+        expect(@client_account.ledger.particulars.first.branch_id).to eq(2)
+        expect(@client_account.ledger.particulars.first.voucher.branch_id).to eq(2)
+        expect(@other_particular.branch_id).to eq(2)
+      end
     end
+
+    context "when voucher have other client particulars" do
+      it 'should move only the mentioned client particulars' do
+        UserSession.selected_fy_code = 7475
+        subject.move_transactions(@client_account, 2, nil, false)
+        expect(Bill.unscoped.where(client_account_id: @client_account.id).first.branch_id).to eq(2)
+        expect(Settlement.where(client_account_id: @client_account.id).first.branch_id).to eq(2)
+        expect(@client_account.ledger.particulars.first.branch_id).to eq(2)
+        expect(@client_account.ledger.particulars.first.voucher.branch_id).to eq(1)
+        expect(@other_particular.branch_id).to eq(1)
+      end
+    end
+
   end
 
   describe 'patch client branch' do
