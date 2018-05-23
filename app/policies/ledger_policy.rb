cfd7d3c02cf9a9caa3cbf7011cb47605b85406b0
subas45 <poudel.subas089@gmail.com>
2018-05-23 11:36:22 +0545

    added the restricted

--- app/policies/ledger_policy.rb
+++ app/policies/ledger_policy.rb
@@ -1,12 +1,13 @@
 class LedgerPolicy < ApplicationPolicy
   # three actions in menu
-  permit_conditional_access_to_employee_and_above :index, :group_members_ledgers, :new
+  permit_conditional_access_to_employee_and_above :index, :group_members_ledgers, :new, :restricted
 
   # hidden menu item
   permit_custom_access :employee_and_above, new_ledger_path, [:create, :update, :edit, :destroy]
 
   permit_custom_access :employee_and_above, group_member_ledgers_path, [:transfer_group_member_balance]
   permit_custom_access :employee_and_above, ledgers_path, [:show, :combobox_ajax_filter]
+  permit_custom_access :employee_and_above, restricted_ledgers_path, [ :toggle_restriction]
 
   def combobox_ajax_filter?
     employee_and_above?
