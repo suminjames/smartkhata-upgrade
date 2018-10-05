922b08dbdeb0ea541224bc46e695f16a88289132
ran010 <ranjanbajra@gmail.com>
2018-10-05 12:43:02 +0545

    unwanted code remove

--- spec/models/vouchers/setup_spec.rb
+++ spec/models/vouchers/setup_spec.rb
@@ -9,10 +9,6 @@ RSpec.describe Vouchers::Setup do
   let(:purchase_bill) { create(:purchase_bill, client_account: client_account, net_amount: 3000) }
   let(:sales_bill) { create(:sales_bill, client_account: client_account, net_amount: 2000) }
 
-  before do
-    expect(Ledger.pluck(:name)).to include("Cash")
-  end
-
   describe "basic vouchers" do
     it "should build  a journal voucher" do
       voucher,
