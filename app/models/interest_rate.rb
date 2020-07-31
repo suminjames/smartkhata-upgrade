0c2e8719731336d2264dac5bca38016dbc670cf7
Rajan Bhattarai <cdrrazan@gmail.com>
2020-07-31 19:17:25 +0545

    Validation on Interest Rate
    Added a between range and interest type validation for interest rate.

--- app/models/interest_rate.rb
+++ app/models/interest_rate.rb
@@ -12,15 +12,31 @@
 #
 
 class InterestRate < ActiveRecord::Base
-  
+
   validates_inclusion_of :rate, in: 1..100, message: "Rate should lie between 1 and 100."
+  validates_presence_of :start_date, :end_date
+
   validate :validate_date_range
-  
+  validate :validate_interest_rate_overlap
+
   enum interest_type: %i[payable receivable]
-  
+
+  def interest_period
+    start_date..end_date
+  end
+
   private
-  
+
+  def validate_interest_rate_overlap
+    other_interest_rates = InterestRate.all
+    is_overlapping = other_interest_rates.any? do |oir|
+      interest_period.overlaps?(oir.interest_period) && interest_type == oir.interest_type
+    end
+    errors.add :start_date, "A interest rate record in the given date range already exists!" if is_overlapping
+  end
+
   def validate_date_range
     errors.add :start_date, "Start date should be before the end date" if start_date >= end_date
   end
 end
+
