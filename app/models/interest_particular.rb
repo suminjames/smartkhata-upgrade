9aef51d071054c0d7c619309aea58dc89811d4b3
Rajan Bhattarai <cdrrazan@gmail.com>
2020-08-17 11:07:50 +0545

    Refactor for overall interest generation for interest_particulars

--- app/models/interest_particular.rb
+++ app/models/interest_particular.rb
@@ -3,23 +3,29 @@ class InterestParticular < ActiveRecord::Base
 
   enum interest_type: %i[dr cr]
 
+  scope :between_record_range, -> (current_date, start_fy_date, end_fy_date) {
+    where "('#{current_date}' BETWEEN #{start_fy_date} AND #{end_fy_date})"
+  }
+
+  def ledger_id
+    ledger.id
+  end
+
   def self.calculate_interest(date)
+    beginning_fy_date = fiscal_year_first_day(get_fy_code(date))
+    ending_fy_date = fiscal_year_last_day(get_fy_code(date))
+
     interest_particulars = ClientAccount.to_a.map do |ca|
       ledger = ca.ledger
-      
-      # SQL query returns either positive or negative value based on the difference
+
       particular_amount = InterestCalculator.call(ledger, date)
-      
-      # NOT SURE what to put here.
-      #
-      # interest_type =
-      #
-      # interest_rate =
-      #
-      # Also the business logic to calculate the interest for single ledger
-      # calculated_interest =   
-      
-      InterestParticular.new(amount: calculated_interest, rate: interest_rate.rate, date: date, interest_type: interest_type, ledger_id: ca.ledger.id )
+      interest_type = particular_amount.positive? ? 'cr' : 'dr'
+      interest_condition = particular_amount.positive? ? 'receivable' : 'payable'
+      applicable_interest_rate = InterestRate.between_record_range(date, beginning_fy_date, ending_fy_date).where(interest_type: interest_condition).first
+
+      calculated_interest = ( particular_amount.abs.to_f * interest_rate.to_f) / 100.0
+
+      InterestParticular.new(amount: calculated_interest, rate: applicable_interest_rate.rate, date: date, interest_type: interest_type, ledger_id: ca.ledger_id )
     end
 
     InterestParticular.import interest_particulars, batch: 200
--- app/services/interest_calculator.rb
+++ app/services/interest_calculator.rb
@@ -11,10 +11,9 @@ class InterestCalculator
     ending_fy_date = fiscal_year_last_day(get_fy_code(@date))
     query =
       <<-SQL
-        SELECT (debit_particular - credit_particular) FROM (SELECT SUM( CASE WHEN transaction_type = 0 Then amount ELSE 0 END ) AS debit_particular,
-        SUM( CASE WHEN transaction_type = 1 THEN amount ELSE 0 END ) as credit_particular
+        SELECT SUM(CASE WHEN transaction_type = 0 THEN amount ELSE amount * -1 END)
         FROM particulars
-        WHERE ( ledger_id = #{@ledger.id}) AND ( value_date BETWEEN '#{beginning_fy_date}' AND '#{ending_fy_date}' )) AS particular_total;
+        WHERE (ledger_id = #{@ledger.id}) AND (value_date BETWEEN '#{beginning_fy_date}' AND '#{ending_fy_date}');
       SQL
     ActiveRecord::Base.connection.exec_query(query).first.values.first
   end
