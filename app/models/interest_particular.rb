1140c77dac648b20fc30c875b890ebdf25f7ce35
Rajan Bhattarai <cdrrazan@gmail.com>
2020-08-17 11:18:39 +0545

    Refactor and typos fix

--- app/models/interest_particular.rb
+++ app/models/interest_particular.rb
@@ -12,20 +12,20 @@ class InterestParticular < ActiveRecord::Base
   end
 
   def self.calculate_interest(date)
-    beginning_fy_date = fiscal_year_first_day(get_fy_code(date))
-    ending_fy_date = fiscal_year_last_day(get_fy_code(date))
+    fy_first_day = fiscal_year_first_day(get_fy_code(date))
+    fy_last_day = fiscal_year_last_day(get_fy_code(date))
 
     interest_particulars = ClientAccount.to_a.map do |ca|
       ledger = ca.ledger
 
-      particular_amount = InterestCalculator.call(ledger, date)
-      interest_type = particular_amount.positive? ? 'cr' : 'dr'
-      interest_condition = particular_amount.positive? ? 'receivable' : 'payable'
-      applicable_interest_rate = InterestRate.between_record_range(date, beginning_fy_date, ending_fy_date).where(interest_type: interest_condition).first
+      particular_net_sum = InterestCalculator.call(ledger, date)
+      interest_type = particular_net_sum.positive? ? 'cr' : 'dr'
+      interest_condition = particular_net_sum.positive? ? 'receivable' : 'payable'
+      applicable_interest_rate = InterestRate.between_record_range(date, fy_first_day, fy_last_day).where(interest_type: interest_condition).first
 
-      calculated_interest = ( particular_amount.abs.to_f * interest_rate.to_f) / 100.0
+      calculated_interest_amount = ( particular_net_sum.abs.to_f * applicable_interest_rate.to_f) / 100.0
 
-      InterestParticular.new(amount: calculated_interest, rate: applicable_interest_rate.rate, date: date, interest_type: interest_type, ledger_id: ca.ledger_id )
+      InterestParticular.new(amount: calculated_interest_amount, rate: applicable_interest_rate.rate, date: date, interest_type: interest_type, ledger_id: ca.ledger_id )
     end
 
     InterestParticular.import interest_particulars, batch: 200
