bffae9ebfecc33790b837faf7c8fd927c42d61c1
Rajan Bhattarai <cdrrazan@gmail.com>
2020-08-17 08:54:58 +0545

    WIP refactor and replacing for the biz logic

--- app/models/interest_particular.rb
+++ app/models/interest_particular.rb
@@ -6,10 +6,19 @@ class InterestParticular < ActiveRecord::Base
   def self.calculate_interest(date)
     interest_particulars = ClientAccount.to_a.map do |ca|
       ledger = ca.ledger
+      
+      # SQL query returns either positive or negative value based on the difference
       particular_amount = InterestCalculator.call(ledger, date)
-      # interest_type = particular_amount.positive?  ? 'dr' : 'cr'
-      # interest_rate = InterestRate.where("date >= start_date AND date <= end_date").where(interest_type: (interest_type == 'dr') ? "payable" : "receivable").first
-      # calculated_interest = ( particular_amount.to_f * interest_rate.to_f ) / 100.0
+      
+      # NOT SURE what to put here.
+      #
+      # interest_type =
+      #
+      # interest_rate =
+      #
+      # Also the business logic to calculate the interest for single ledger
+      # calculated_interest =   
+      
       InterestParticular.new(amount: calculated_interest, rate: interest_rate.rate, date: date, interest_type: interest_type, ledger_id: ca.ledger.id )
     end
 
