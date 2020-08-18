1dd8b158111246d2d104af3bdea0d6d45bd39bed
Rajan Bhattarai <cdrrazan@gmail.com>
2020-08-18 09:32:56 +0545

    Refactor and fixes from PR revision

--- db/migrate/20200803134806_add_value_date_to_particulars.rb
+++ db/migrate/20200803134806_add_value_date_to_particulars.rb
@@ -1,10 +1,6 @@
 class AddValueDateToParticulars < ActiveRecord::Migration
   def change
     add_column :particulars, :value_date, :date
-    
-    reversible do |dir|
-      dir.up { Particular.update_all('value_date = transaction_date') }
-    end
   end
 end
 
