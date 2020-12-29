7440fe50d222eb8d5c224aad7740f96c1b88bde5
subzekt <poudel.subas089@gmail.com>
2020-12-29 09:58:43 +0545

    interest calculation changes

--- app/jobs/interest_job.rb
+++ app/jobs/interest_job.rb
@@ -6,7 +6,7 @@ class InterestJob < ActiveJob::Base
     value_date = value_date.to_date
     fy_code = get_fy_code(value_date)
     final_date =  fiscal_year_last_day(fy_code)
-    (value_date .. [value_date,final_date].min).each do |date|
+    (value_date .. [value_date,final_date, Date.current].min).each do |date|
       ActiveRecord::Base.transaction do
         InterestParticular.calculate_interest(date: date, ledger_id: ledger_id)
       end
