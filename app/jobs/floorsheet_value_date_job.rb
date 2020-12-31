742c4a6ba82e54d0f1f3b351d94faa8004f556f0
subzekt <poudel.subas089@gmail.com>
2020-12-31 22:06:15 +0545

    fix for value date

--- app/jobs/floorsheet_value_date_job.rb
+++ app/jobs/floorsheet_value_date_job.rb
@@ -2,8 +2,13 @@ class FloorsheetValueDateJob < ActiveJob::Base
 
   queue_as :default
   def perform(report_date, value_date)
-    voucher_ids = BillVoucherAssociation.where(bill_id: Bill.where(date: report_date).select(:id)).select(:voucher_id)
-    Particular.where(voucher_id: voucher_ids).find_each do |particular|
+    voucher_ids = BillVoucherAssociation.where(bill_id: Bill.purchase.where(date: report_date).select(:id)).select(:voucher_id)
+
+    default_ledger_names = ["Purchase Commission", "Sales Commission", "Nepse Purchase", "Nepse Sales","TDS","DP Fee/ Transfer"]
+    Particular
+      .where(voucher_id: voucher_ids)
+      .where.not(ledger_id: Ledger.where(name: default_ledger_names).select(:id))
+      .find_each do |particular|
       particular.update(value_date: value_date)
     end
   end
